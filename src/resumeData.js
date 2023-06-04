f4a7b5ada9d0c5f682f8af3d1ff52be85ea5211d
Syed Misbah Ur Rehman <syedmisbah588@gmail.com>
2023-06-04 23:49:12 +0500

    First commit

---
+++ src/resumeData.js
@@ -0,0 +1,56 @@
+let resumeData = {
+    // "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Syed Muhammad Abeed Ur Rehman",
+    "role": "Knitting Expert",
+    "linkedinId":"Your LinkedIn Id",
+    "roleDescription": " General Manager Knitting Ops",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/syed-abeed/",
+          "className":"fa fa-linkedin"
+        },
+       
+       
+      ],
+    "aboutme":"I am a highly experienced fabric manufacturing professional with a strong track record in producing a wide range of fabrics for renowned clients. My expertise includes manufacturing fabrics like Jerseys, Piques, thermals, fleeces, ribs, and semi-jacquard, among others. I have a deep understanding of fabric aesthetics and the ability to achieve specific characteristics. I excel in team management and project coordination, with a focus on mentoring junior team members. I am skilled in product development and replication, capable of recreating fabrics from around the world. In optimizing knitting plant operations, I minimize dependency on external knitters and ensure efficient utilization of machinery. With my strong leadership skills and knowledge, I deliver high-quality fabrics while maintaining budget control.",
+    "address":"Karachi, Pakistan",
+    "education":[
+      {
+        "UniversityName":"NED University of Eng. & Tech, Karachi",
+        "specialization":"B.E. (Mechanical)",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"1993",
+      },
+     
+    ],
+    "work":[
+      {
+        "CompanyName":"Alkaram Textile Mills Ltd, Karachi",
+        "specialization":"General Manager Knitting Ops",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2019 - present",
+        "Achievements":"Planned & Executed the Knitting of approx. 1500K kg fabric on monthly basis         Developed and managed the knitting dept of 60 Circular knitting machines         Set the Quality Parameters and specs sheet and their implementation at in-house and outside knitting        depts         Provided technical support to Production, Maintenance and Quality Teams for ensuring optimum        production with quality         Provided assistance to PD and Marketing Dept for new developments keeping in view the sustainability,        productivity and cost effectiveness, meeting the customer’s expectations         Coordinated with Dyeing and cutting dept to fine tune the fabric parameters thus minimizing the        shortfalls         Participated in cross-functional teams to execute and implement improvement plans like 5S and Kaizen         Trained and groomed the team members"
+      },
+      {
+        "CompanyName":"Lucky Knits (Pvt) Ltd, Karachi",
+        "specialization":"DGM Knitting",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2019",
+        "Achievements":" Planned &amp; executed approx 600K kg knitted fabric on monthly basis         Managed knitting dept of 59 Circular knitting machines, 13 Flat knitting machines and 38 Braiding        machines; also commercial knitters         Set Quality Parameters and developed Knitting Specs Sheet having Greige Parameters for each Buyer         Headed Knitting NPD Dept and Developed many New Fabrics         Provided technical support to Production, Quality and Maintenance Dept for improvement and better        utilization of man and machine         Coordinated with Merchandising, dyeing and cutting dept to optimize the fabrication process        Trained and groomed the workers and staff"
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"MS OFFICE"
+      },
+      {
+        "skillname":"ORACLE"
+      }
+      
+    ],
+   
+  }
+  
+  export default resumeData
