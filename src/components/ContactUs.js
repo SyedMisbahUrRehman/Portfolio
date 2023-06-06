ca8deba35123a9ae194c1099c9bada9f5670914c
Syed Misbah Ur Rehman <syedmisbah588@gmail.com>
2023-06-06 18:30:10 +0500

    Warnings fixed

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 export default class ContactUs extends Component {
   render() {
-    let resumeData = this.props.resumeData;
+    // let resumeData = this.props.resumeData;
     return (
       <section id="contact">
           <div className="row section-head">
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,7 +6,7 @@ export default class Porfolio extends Component {
       <section id="portfolio">
       <div className="row">
         <div className="twelve columns collapsed">
-    <p>For works<a href="https://drive.google.com/file/d/1pDCSx5bWKY5Dnvei4-pf2web70ujA_aN/view?usp=sharing" target="_blank"> click here</a>
+    <p>For works<a href="https://drive.google.com/file/d/1pDCSx5bWKY5Dnvei4-pf2web70ujA_aN/view?usp=sharing" target="_blank" rel="noopener noreferrer" > click here</a>
 .</p>
  
           <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
@@ -16,7 +16,7 @@ export default class Porfolio extends Component {
                 <div className="columns portfolio-item">
                   <div className="item-wrap">
                     <a href="#modal-01">
-                      <img src={`${item.imgurl}`} className="item-img"/>
+                      {/* <img src={`${item.imgurl}`} className="item-img"/> */}
                       <div className="overlay">
                         <div className="portfolio-item-meta">
                           <h5>{item.name}</h5>
