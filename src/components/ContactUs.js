8d83c98562755bb7da100dbb1f2d281663b44819
Syed Misbah Ur Rehman <syedmisbah588@gmail.com>
2023-06-06 18:32:38 +0500

    contact updated

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -7,7 +7,7 @@ export default class ContactUs extends Component {
           <div className="row section-head">
             <div className="ten columns">
               <p className="lead">
-              Feel free to contact Us
+              Feel free to contact me
               </p>
             </div>
           </div>
